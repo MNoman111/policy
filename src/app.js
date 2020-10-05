048ed5085c28969aab72b060b08468d193f3ecdc
Noman Naeem <MNoman111@gmail.com>
2020-10-05 06:56:41 +0500

    policy

--- src/app.js
+++ src/app.js
@@ -1,7 +1,6 @@
 const path = require("path")
 const express = require("express");
 const hbs = require("hbs")
-const darkSky = require("./utils/darkSky")
 const app = express()
 const port = process.env.PORT || 3000;
 
@@ -19,60 +18,11 @@ hbs.registerPartials(partialsPath);
 app.use(express.static( publicPath ))
 
 app.get( "", (req, res) => {
-    res.render("index", {
-        title: "Index",
-        name: "Noman Naeem"
-    })
 } )
 
-app.get( "/about", (req, res) => {
-    res.render("about", {
-        title: "About",
-        name: "Noman Naeem"
-    })
+app.get( "/foodella/policy", (req, res) => {
+    res.render("policy")
 } )
-
-app.get( "/help", (req, res) => {
-    res.render( "help", {
-        title: "Help",
-        name: "Noman Naeem"
-    } )
-} )
-
-app.get( "/weather", (req, res) => {
-    
-    if(!req.query.address){
-        return res.send({
-            error: "No address provided."
-        })
-    }
-
-    darkSky( req.query.address, (error, urlWeather) => {        
-        if( error ){
-            console.log(error);
-            
-            return res.send({
-                error
-            })
-        }else{            
-            res.send({
-                location: "Karachi",
-                address: req.query.address,
-                forecast: urlWeather
-            })
-        }
-    })
-    console.log('hjk');
-    
-} )
-
-app.get( "/help/*", (req, res) => {
-    res.render("404", {
-        title: "404",
-        error: "Help Article Not Found.",
-        name: "Noman Naeem"
-    })
-})
 
 app.get( "*", (req, res) => {
     res.render("404", {
